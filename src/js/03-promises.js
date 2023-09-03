99fc11da7473323814e0ad15c622b455321aca1c
Eduard <sorokolietov@gmail.com>
2023-09-03 23:26:04 +0200

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,18 +13,40 @@ function handleSubmit(e) {
 
   let id = 1;
 
-  setTimeout(() => {
-    const intervalID = setInterval(() => {
+  const timeoutId = setTimeout(() => {
+
+    if (id > amountRef) {
+      clearTimeout(timeoutId);
+      return;
+    }
+
+    let delay = Number(delayRef) + Number((id - 1) * stepRef);
+
+    console.log(delayRef);
+    console.log(stepRef);
+    console.log(delay);
+
+    createPromise(id, delay)
+      .then(({position, delay}) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+
+    id += 1;
+
+    const intervalId = setInterval(() => {
       if (id > amountRef) {
-        clearInterval(intervalID);
+        clearInterval(intervalId);
         return;
       }
 
       let delay = Number(delayRef) + Number((id - 1) * stepRef);
 
+      console.log(delayRef);
+      console.log(stepRef);
+      console.log(delay);
+
       createPromise(id, delay)
         .then(({position, delay}) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
-        .catch(({position, delay}) => Notify.failure(`✅ Fulfilled promise ${position} in ${delay}ms`));
+        .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
       
       id += 1;
     }, stepRef)
